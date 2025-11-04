b3deb77eab7138d6efc55efa08074fbdd969df4d
Daniel Riverol <dannyriverol@hotmail.com>
2025-11-04 20:27:41 -0300

    users

--- hands-on_clase7/src/controllers/users.controller.js
+++ hands-on_clase7/src/controllers/users.controller.js
@@ -1,10 +1,34 @@
+import Users from "../dao/classes/users.dao.js";
+
+const userService = new Users();
 export const getUsers = async (req, res) => {
-  res.status(200).json({ status: "success", message: "Get Users" });
+  try {
+    const users = await userService.getUsers();
+    res.status(200).json({ status: "success", payload: users });
+    // hagamos un ststau 400
+  } catch (error) {
+    return res.status(500).json({ status: "Error", error: error.message });
+  }
 };
 export const getUserById = async (req, res) => {
-  const id = req.params.id;
-  res.status(200).json({ status: "success", message: `Get User By ID: ${id}` });
+  const uid = req.params.uid;
+  try {
+    const user = await userService.getUserById(uid);
+    user
+      ? res.status(200).json({ status: "success", payload: user })
+      : res
+          .status(400)
+          .json({ status: "error", message: "no se encuentra el usuario" });
+  } catch (error) {
+    return res.status(500).json({ status: "Error", error: error.message });
+  }
 };
 export const saveUser = async (req, res) => {
-  res.status(201).json({ status: "success", message: "Save User" });
+  const user = req.body;
+  try {
+    const newUser = await userService.createUser(user);
+    res.status(201).json({ status: "success", payload: newUser });
+  } catch (error) {
+    return res.status(500).json({ status: "Error", error: error.message });
+  }
 };
--- hands-on_clase7/src/dao/classes/users.dao.js
+++ hands-on_clase7/src/dao/classes/users.dao.js
@@ -1,3 +1,41 @@
 import usersModel from "../models/users.model.js";
 
-export default class Users {}
+export default class Users {
+  getUsers = async () => {
+    try {
+      const users = await usersModel.find();
+      return users;
+    } catch (error) {
+      console.error(error.message);
+      return null;
+    }
+  };
+  getUserById = async (uid) => {
+    try {
+      const user = await usersModel.findOne({ _id: uid });
+      return user;
+    } catch (error) {
+      console.error(error.message);
+      return null;
+    }
+  };
+  createUser = async (user) => {
+    try {
+      const newUser = await usersModel.create(user);
+      return newUser;
+    } catch (error) {
+      return null;
+    }
+  };
+  updateUser = async (uid, user) => {
+    try {
+      const updatedUser = await usersModel.updateOne(
+        { _id: uid },
+        { $set: user }
+      );
+      return updatedUser;
+    } catch (error) {
+      return null;
+    }
+  };
+}
