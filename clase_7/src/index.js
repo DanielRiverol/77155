940ed0aaacd1068f7b9e37a48e4b9aa8fda58f65
Daniel Riverol <dannyriverol@hotmail.com>
2025-11-11 21:11:49 -0300

    clase nodemailer

--- clase_7/src/index.js
+++ clase_7/src/index.js
@@ -1,21 +1,54 @@
-import express from 'express'
+import express from "express";
 import __dirname from "./utils/index.js";
+import nodemailer from "nodemailer";
 //settings
-const app = express()
-app.set('PORT', 3000)
+const app = express();
+app.set("PORT", 3000);
 
+const transport = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.MAIL_USERNAME,
+    pass: process.env.MAIL_PASSWORD,
+  },
+});
 
 // middlewares
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
 //routes
-app.get('/',(req,res)=>{
-    res.json({title: 'Home Page'})
-})
+app.get("/", (req, res) => {
+  res.json({ title: "Home Page" });
+});
 
+// mail
+app.get("/mail", async (req, res) => {
+  // const {email}=req.body
+  const { email } = req.query;
+  const result = await transport.sendMail({
+    from: `Correo de prueba <${process.env.MAIL_USERNAME}>`,
+    to: email,
+    subject: "La verdad sobre el backend",
+    html: `<div>
+              <h1>La verdad sobre el backend</h1>
+              
+           <p>Hola ${email} mira lo que te enviamos ${process.env.HOST_URL}/recupero-password</p> 
+          </div>`,
+    attachments: [
+      {
+        filename: "img1.jpg",
+        path: "./src/public/img/img1.jpg",
+        cid: "img1",
+      },
+    ],
+  });
+  res.send("Correo enviado");
+});
 //listeners
-app.listen(app.get("PORT"),()=>{
-    console.log(`Server on port http://localhost:${app.get("PORT")}`);
-})
+app.listen(app.get("PORT"), () => {
+  console.log(`Server on port http://localhost:${app.get("PORT")}`);
+});
